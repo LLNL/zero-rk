006f62f221b9e665a7197fe27d859d31ae3a00e8
Russell Whitesides <whitesides1@llnl.gov>
2024-01-31 11:07:42 -0800

    [gpu] : Change default arch and allow for override

--- cmake/gpu.cmake
+++ cmake/gpu.cmake
@@ -3,9 +3,11 @@ option(ENABLE_GPU "Enable CUDA libraries and applications." OFF)
 cmake_dependent_option(ENABLE_MAGMA "Enable MAGMA GPU library" ON "ENABLE_GPU" OFF)
 if(${ENABLE_GPU})
   set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
-  enable_language(CUDA)
   set(CMAKE_CUDA_STANDARD 14)
-  set(CMAKE_CUDA_ARCHITECTURES "60;61;75")
+  if(NOT DEFINED CMAKE_CUDA_ARCHITECTURES)
+    set(CMAKE_CUDA_ARCHITECTURES 70)
+  endif()
+  enable_language(CUDA)
   #N.B. this is for configuring external projects that use CMAKE_CUDA_ARCHITECTURES
   #     as semi-colon separated lists get munged when passing through ExternalProject_Add
   string(REPLACE ";" "|" CMAKE_CUDA_ARCHITECTURES_EXT "${CMAKE_CUDA_ARCHITECTURES}")
