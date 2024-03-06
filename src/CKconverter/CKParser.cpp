3b5464b494ef32e37ea0bc371f42c05f16f7830e
Russell Whitesides <whitesides1@llnl.gov>
2024-03-06 14:37:42 -0800

    parser: throw error on negative Arrhenius A-factor

--- src/CKconverter/CKParser.cpp
+++ src/CKconverter/CKParser.cpp
@@ -1370,10 +1370,11 @@ next:
                 rxn.kf.E = de_atof(toks[ntoks - 1]);
 
                 // 2/10/03: allow negative prefactor but print a warning
-                if (rxn.kf.A < 0.0) 
-                    *m_log << "Warning: negative prefactor at line " 
-                           << m_line << endl;
-                //throw CK_SyntaxError(*m_log, "negative prefactor", m_line);
+		// 3/06/24: don't allow negative prefactor
+                if (rxn.kf.A < 0.0)
+                //    *m_log << "Warning: negative prefactor at line "
+                //           << m_line << endl;
+                    throw CK_SyntaxError(*m_log, "negative prefactor", m_line);
 
                 if (debug) *m_log << "Processing products..." << sright << endl;
                 sright = sright.substr(0, sright.find(toks[ntoks - 3]) - 1 );
